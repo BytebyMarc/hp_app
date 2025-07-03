3a1d639e734e091bbad6583cc82f5e7115540045
Marc <marcboehnert1986@gmail.com>
2025-07-03 15:22:27 +0200

    add CategoryQuestionPage to show questions from a category

---
+++ lib/bloc/blocs/category_question_page_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:hp_app/bloc/blocs/question_bloc_base.dart';
+import 'package:hp_app/models/question_model.dart';
+
+class CategoryQuestionPageBloc extends QuestionBlocBase {
+
+
+
+
+  @override
+  Future<QuestionModel> fetchNextQuestion() {
+    // TODO: implement fetchNextQuestion
+    throw UnimplementedError();
+  }
+
+
+
+
+
+}
--- lib/pages/category_question_page.dart
+++ lib/pages/category_question_page.dart
@@ -1,25 +1,29 @@
-import 'package:hp_app/bloc/blocs/question_bloc_base.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hp_app/models/question_model.dart';
+import '../bloc/blocs/category_question_page_bloc.dart';
+import '../bloc/blocs/false_question_bloc.dart';
+import '../repository/get_question.dart';
+import '../widgets/views/question_view.dart';
 
-class CategoryQuestionPage extends QuestionBlocBase {
+class CategoryQuestionPage extends StatelessWidget {
+  final int catID;
 
-
-
-
-
-
-
-
-
-
-
+  const CategoryQuestionPage({super.key,  required this.catID });
 
   @override
-  Future<QuestionModel> fetchNextQuestion() {
-    // TODO: implement fetchNextQuestion
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return BlocProvider<CategoryQuestionPageBloc>(
+      create: (_) => CategoryQuestionPageBloc(
+        questionRepo: GetQuestionRepository(),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Kategorien'),
+        ),
+        body: const QuestionView<CategoryQuestionPageBloc>(),
+      ),
+    );
   }
-
-
-
 }
